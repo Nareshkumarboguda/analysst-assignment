3f24a13010200b98ae3320628dcf22c30d18ca84
Nareshkumarboguda <86726895+Nareshkumarboguda@users.noreply.github.com>
2023-01-26 23:11:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import CardContainer from "../src/Components/CardContainer/CardContainer.js";
+export default function App() {
+  return (
+    <div className="App">
+      <CardContainer />
+    </div>
+  );
+}
